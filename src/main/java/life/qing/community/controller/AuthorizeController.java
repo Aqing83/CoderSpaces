58eb0ac3b363264cc6fde3e316a8c6cb04f90b4f
Qing <Qing__sen@163.com>
2020-05-14 10:32:52 +0800

    日常更新

--- src/main/java/life/qing/community/controller/AuthorizeController.java
+++ src/main/java/life/qing/community/controller/AuthorizeController.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * 2020/05/14 0:02
  */
@@ -29,7 +31,8 @@ public class AuthorizeController {
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
-                           @RequestParam(name = "state") String state) {
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -38,7 +41,14 @@ public class AuthorizeController {
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        if(user != null){
+            System.out.println(user);
+            //登录成功，写cookie和session
+            request.getSession().setAttribute("user",user);
+        }else{
+            //登录失败，重新登录
+        }
+        return "redirect:/";
+
     }
 }
