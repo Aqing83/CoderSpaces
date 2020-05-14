7e206869b0979b6c1bbb38e73ba8a75ada437221
Qing <Qing__sen@163.com>
2020-05-14 23:42:55 +0800

    集成flyway

--- src/main/java/life/qing/community/mapper/UserMapper.java
+++ src/main/java/life/qing/community/mapper/UserMapper.java
@@ -10,6 +10,7 @@ import org.apache.ibatis.annotations.Select;
 public interface UserMapper {
     @Insert("insert into USER(name,account_id,token,gmt_create,gmt_modified) values(#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
     void insert(User user);
+
     @Select("select * from user where token = #{token}")
-    User selectToken(@Param("token") String token);
+    User selectToken(@Param("token")String token);
 }
