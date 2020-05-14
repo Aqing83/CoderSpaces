499cd4d4147dc376449877c969ab4bae325f2d67
Qing <Qing__sen@163.com>
2020-05-14 09:51:48 +0800

    常规更新

--- src/main/java/life/qing/community/controller/IndexController.java
+++ src/main/java/life/qing/community/controller/IndexController.java
@@ -6,11 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class HelloController {
-    @GetMapping("/hello")
-    private String hello(@RequestParam(name = "name") String name, Model model) {
-        model.addAttribute("name", name);
-        return "hello";
-
+public class IndexController {
+    @GetMapping("/")
+    private String index(){
+        return "index";
     }
+
 }
