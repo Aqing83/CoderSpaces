b84681876130a1cabe331ff21aa741385ea52fbf
Qing <Qing__sen@163.com>
2020-05-15 21:40:08 +0800

    增加发布问题功能

---
+++ src/main/java/life/qing/community/mapper/QuestionMapper.java
@@ -0,0 +1,11 @@
+package life.qing.community.mapper;
+
+import life.qing.community.model.Question;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface QuestionMapper {
+    @Insert("insert into Question (title,description,gmt_create,gmt_modified,creator,tag)values(#{title},#{description},#{gmtCreate},#{gmtModified},#{creator},#{tag})")
+    void create(Question question);
+}
