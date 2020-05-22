970b3757dc6829723790efcd8dc2eb034b3ad504
Qing <Qing__sen@163.com>
2020-05-22 18:21:56 +0800

    完成GitHub授权登录

--- src/main/java/life/qing/community/dto/AccessTokenDTO.java
+++ src/main/java/life/qing/community/dto/AccessTokenDTO.java
@@ -1,5 +1,8 @@
 package life.qing.community.dto;
 
+import lombok.Data;
+
+@Data
 public class AccessTokenDTO {
     private String client_id;
     private String client_secret;
@@ -7,43 +10,5 @@ public class AccessTokenDTO {
     private String Redirect_uri;
     private String state;
 
-    public String getClient_id() {
-        return client_id;
-    }
 
-    public void setClient_id(String client_id) {
-        this.client_id = client_id;
-    }
-
-    public String getClient_secret() {
-        return client_secret;
-    }
-
-    public void setClient_secret(String client_secret) {
-        this.client_secret = client_secret;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public String getRedirect_uri() {
-        return Redirect_uri;
-    }
-
-    public void setRedirect_uri(String redirect_uri) {
-        Redirect_uri = redirect_uri;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
 }
