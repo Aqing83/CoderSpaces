010f2d791922e8ca1249a2aaf07956be8e735bf4
Qing <Qing__sen@163.com>
2020-06-05 19:12:02 +0800

    增加-不刷新页面登录

--- src/main/java/life/qing/community/exception/CustomizeErrorCode.java
+++ src/main/java/life/qing/community/exception/CustomizeErrorCode.java
@@ -3,7 +3,7 @@ package life.qing.community.exception;
 public enum CustomizeErrorCode implements ICustomizeErrorCode {
     QUESTION_NOT_FOUND(2001, "你找的问题不在了，要不换一个试试？"),
     TARGET_PARAM_NOT_FOUND(2002, "未选中任何问题或评论进行回复"),
-    NO_LOGIN(2004, "当前操作需要登录，请登录后重试"),
+    NO_LOGIN(2003, "当前操作需要登录，请登录后重试"),
     SYS_ERROR(2004, "服务器冒烟了，要不然你稍后再试试！！！    "),
     TYPE_PARAM_WRONG(2005, "评论类型错误或不存在"),
     COMMENT_NOT_FOUND(2006, "回复的评论不存在了，要不换个试试？"),
--- src/main/resources/static/js/community.js
+++ src/main/resources/static/js/community.js
@@ -14,9 +14,16 @@ function post() {
             if(response.code == 200){
                 $("#comment_section").hide();
             }else{
+                if(response.code == 2003){
+                    var isAccepted = confirm(response.message);
+                    if(isAccepted){
+                        window.open("https://gitee.com/oauth/authorize?client_id=fcb2ff4472df1dc6c96ebbe86f7deb00bc79ebe96dafdd5eb62fc744eda301df&redirect_uri=http://localhost:8887/callbackGitEE&response_type=code");
+                        window.localStorage.setItem("closable",true);
+                    }
+                }
+
                 alert(response.message);
             }
-            console.log(response);
 
         },
         dataType: "json"
